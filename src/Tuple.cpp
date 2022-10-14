2d61690b28f62bd5f41a0509174cd275e3cc1d31
cshangjie <62275103+cshangjie@users.noreply.github.com>
2022-10-14 03:24:09 -0500

    feat: add point and vector represention through Tuple class

--- src/Tuple.cpp
+++ src/Tuple.cpp
@@ -1,4 +1,5 @@
 #include "Tuple.hpp"
+#include "Math.hpp"
 #include <string>
 
 Tuple::Tuple(float x, float y, float z, float w) {
@@ -7,3 +8,11 @@ Tuple::Tuple(float x, float y, float z, float w) {
   z_ = z;
   w_ = w;
 };
+
+bool Tuple::IsPoint() const{
+  return equals(1.0, w_);
+}
+
+bool Tuple::IsVector() const{
+  return equals(0.0, w_);
+}
