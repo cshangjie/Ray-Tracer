49b3fca98d80f1f57f05b8d14635d333ddce707f
cshangjie <cshangjie@gmail.com>
2022-10-13 22:20:51 -0500

    feat: Initialize tuple class

---
+++ src/Tuple.cpp
@@ -0,0 +1,9 @@
+#include "Tuple.hpp"
+#include <string>
+
+Tuple::Tuple(float x, float y, float z, float w) {
+  x_ = x;
+  y_ = y;
+  z_ = z;
+  w_ = w;
+};
---
+++ src/Tuple.hpp
@@ -0,0 +1,33 @@
+#ifndef TUPLE_HPP
+#define TUPLE_HPP
+
+class Tuple {
+ public:
+  Tuple() {};
+  Tuple(float x, float y, float z, float w);
+  ~Tuple() = default;
+
+  float x() const { return x_; }
+  float y() const { return y_; }
+  float z() const { return z_; }
+  float w() const { return w_; }
+  void SetW(float w) { w_ = w; };
+
+  bool IsPoint() const;
+  bool IsVector() const;
+
+ private:
+  float x_, y_, z_, w_;
+};
+
+Tuple operator+(const Tuple lhs, const Tuple rhs);
+Tuple operator-(const Tuple lhs, const Tuple rhs);
+Tuple operator*(const Tuple t, float scalar);
+Tuple operator/(const Tuple t, float div);
+Tuple operator-(const Tuple neg);
+bool operator==(const Tuple lhs, const Tuple rhs);
+
+Tuple Point(float x, float y, float z);
+Tuple Vector(float x, float y, float z);
+
+#endif /* TUPLE_HPP */
